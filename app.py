c4908b363310b8ba6a19e6edf593b6ae13e147d3
Sivasuriya-1709 <sivasuriya17092005@gmail.com>
2026-07-25 13:52:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,6 @@
 import os, re, urllib.request , urllib 
+from flask import flask , jsonify , request , Request
+
+app = flask(__name__)
+req = urllib.request(f"https://wwww.youtube.com/search?" methods = {"agents",Mozilla/5.0))
+re = url
